26b58021fc68485af9867fa512e1dc00b207d579
Jesus Castro <jtcastro98@gmail.com>
2021-10-06 19:52:30 -0500

    Finalización de la parte When

--- features/steps/loginSteps.js
+++ features/steps/loginSteps.js
@@ -1,7 +1,9 @@
 const { When, Then, Given } = require('cucumber');
 const { Builder, By, until } = require('selenium-webdriver');
-require('chromedriver');
+const assert = require('assert');
 
+require('chromedriver');
+// setDefaultTimeout(60 * 1000);
  
 Given('I am on the {string} page', async function (string) {
     // Write code here that turns the phrase above into concrete actions
@@ -9,18 +11,19 @@ Given('I am on the {string} page', async function (string) {
     .forBrowser('chrome')
     .build();
     await this.driver.get(`https://the-internet.herokuapp.com/${string}`);
-    });
+});
 
 When('I login with {string} and {string}', function (string, string2) {
     // Write code here that turns the phrase above into concrete actions
     this.driver.findElement(By.id("username")).sendKeys(string);
     this.driver.findElement(By.id("password")).sendKeys(string2);
-    return 'pending';  
-  });
+    this.driver.findElement(By.className('radius')).click(); 
+});
 
-Then('I should see a message saying {string}', function (string) 
+Then('I should see a message saying {string}', async function (string) 
 {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+  await this.driver.wait(until.elementLocated(By.id('flash')))  
+  let message = await this.driver.findElement(By.id('flash')).getText();
+  assert.notEqual(string, message.substring(0, message.indexof("!") + 1));
+});
 
